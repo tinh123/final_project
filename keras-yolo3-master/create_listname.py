eaf46aaea703c4f831062c5e15aed578787f3774
tinh123 <thanhtinh160896@gmail.com>
2018-09-28 11:35:09 +0700

    change create list data for train, test, val

--- keras-yolo3-master/create_listname.py
+++ keras-yolo3-master/create_listname.py
@@ -1,11 +1,27 @@
 import os
 def main():
-    sets = ['train','test', 'val']
-    for set_ in sets:
-        with open('VOCdevkit/VOC2018/ImageSets/Main/'+set_+'.txt','w') as f:
-            l=os.listdir('VOCdevkit/VOC2018/data/'+set_)
-            for i in l:
-                li=i.split('.')[0] 
-                f.writelines(li+'\n')
+    f_train = open('VOCdevkit/VOC2018/ImageSets/Main/train.txt','w')
+    f_test = open('VOCdevkit/VOC2018/ImageSets/Main/test.txt','w')
+    f_val = open('VOCdevkit/VOC2018/ImageSets/Main/val.txt','w')
+    l = os.listdir('VOCdevkit/VOC2018/Annotations/')
+    for i in range(len(l)):
+        li = l[i].split('.')[0]
+        if i <= 200:
+            f_val.writelines(li+'\n')
+        elif i>200 and i<600:
+            f_test.writelines(li+'\n')
+        else:
+            f_train.writelines(li+'\n')
+    f_train.close()
+    f_test.close()
+    f_val.close()
+        
+    # sets = ['train','test', 'val']
+    # for set_ in sets:
+    #     with open('VOCdevkit/VOC2018/ImageSets/Main/'+set_+'.txt','w') as f:
+    #         l=os.listdir('VOCdevkit/VOC2018/data/'+set_)
+    #         for i in l:
+    #             li=i.split('.')[0] 
+    #             f.writelines(li+'\n')
 if __name__=="__main__":
     main()
